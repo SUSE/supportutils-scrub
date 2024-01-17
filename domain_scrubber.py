4502539295a02fe07310772a92372f42c535d574
Ronald Pina <rpina@suse.com>
2024-01-17 01:53:36 +0100

    Some improvements in domain scrub logic.

--- domain_scrubber.py
+++ domain_scrubber.py
@@ -12,3 +12,25 @@ class DomainScrubber:
             text = text.replace(domain, obfuscated)
         return text
     
+    def extract_domains_from_section(file_name, section_start):
+        pattern = r'\b(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}\b'
+        domains = []
+        try:
+            with open(file_name, 'r') as file:
+                section_found = False
+                for line in file:
+                    if section_start in line:
+                        section_found = True
+                        continue 
+                    # Check if the line is the start of a new section, indicating the end of the current section
+                    if section_found and "#==[" in line:
+                        break
+                    # Extract domains from the line if we are in the correct section
+                    if section_found:
+                        found_domains = re.findall(pattern, line)
+                        domains.extend(found_domains)
+
+        except FileNotFoundError:
+            print(f"File not found: {file_name}")
+
+        return domains
--- extractor.py
+++ extractor.py
@@ -3,9 +3,9 @@
 import os
 import shutil
 import logging
+import tarfile 
 
-
-def extract_supportconfig(supportconfig_path):
+def extract_supportconfig(supportconfig_path, logger):
     """
     Extract Supportconfig files and return a list of  files.
     """
@@ -15,8 +15,9 @@ def extract_supportconfig(supportconfig_path):
         # Supportconfig is a folder
         report_files = walk_supportconfig(supportconfig_path)
     elif supportconfig_path.endswith(".txz"):
-        # Supportconfig is an XZ archive
-        report_files = extract_xz_archive(supportconfig_path)
+        report_files = extract_xz_archive(supportconfig_path, logger)
+    elif supportconfig_path.endswith(".tgz"):
+        report_files = extract_tgz_archive(supportconfig_path, logger)
     else:
         logging.error(f"Unsupported file type: {supportconfig_path}")
         raise Exception(f"Unsupported file type: {supportconfig_path}")
@@ -35,7 +36,7 @@ def walk_supportconfig(folder_path):
                 report_files.append(os.path.join(root, file))
     return report_files
 
-def extract_xz_archive(archive_path):
+def extract_xz_archive(archive_path, logger):
     """
     Extract an XZ archive and return a list of report files.
     """
@@ -43,5 +44,18 @@ def extract_xz_archive(archive_path):
     unpack_folder = shutil.unpack_archive(archive_path, extract_base_folder)
     supportconfig_folder = os.path.splitext(archive_path)[0]
     report_files = walk_supportconfig(supportconfig_folder)
+    logger.info(f"Extracted .txz to: {supportconfig_folder}")
+
     return report_files
 
+def extract_tgz_archive(archive_path, logger):
+    """Extract a .tgz (tar gzipped) archive and return a list of report files."""
+    extract_base_folder = os.path.dirname(archive_path)
+    with tarfile.open(archive_path, "r:gz") as tar:
+        tar.extractall(path=extract_base_folder)
+        # Assuming the first member is the root directory
+        root_directory = tar.getmembers()[0].name.split('/')[0]
+    supportconfig_folder = os.path.join(extract_base_folder, root_directory)
+    logger.info(f"Extracted .tgz to: {supportconfig_folder}")
+    report_files = walk_supportconfig(supportconfig_folder)
+    return report_files
