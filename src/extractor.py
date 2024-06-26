03924b3d4997ba9bd3d0d0008800b4a4746a57f8
Ronald Pina <rpina@suse.com>
2024-06-26 19:26:26 +0200

    Adding compressing logic, and doing some improvement in the logging.

--- src/extractor.py
+++ src/extractor.py
@@ -41,21 +41,46 @@ def extract_xz_archive(archive_path, logger):
     Extract an XZ archive and return a list of report files.
     """
     extract_base_folder = os.path.dirname(archive_path)
-    unpack_folder = shutil.unpack_archive(archive_path, extract_base_folder)
-    supportconfig_folder = os.path.splitext(archive_path)[0]
-    report_files = walk_supportconfig(supportconfig_folder)
-    logger.info(f"Extracted .txz to: {supportconfig_folder}")
+    base_name = os.path.basename(archive_path)
+    base_name_no_ext = os.path.splitext(base_name)[0]
+    clean_folder_name = base_name_no_ext + "_clean"
+    clean_folder_path = os.path.join(extract_base_folder, clean_folder_name)
 
-    return report_files
+    if os.path.exists(clean_folder_path):
+        shutil.rmtree(clean_folder_path)
+
+    with tarfile.open(archive_path, 'r:xz') as tar:
+        members = tar.getmembers()
+        for member in members:
+            member.name = os.path.join(clean_folder_name, os.path.basename(member.name))
+            tar.extract(member, path=extract_base_folder)
+    
+    report_files = walk_supportconfig(clean_folder_path)
+    logger.info(f"Extracted .txz to: {clean_folder_path}")
+
+    return report_files, clean_folder_path
 
 def extract_tgz_archive(archive_path, logger):
     """Extract a .tgz (tar gzipped) archive and return a list of report files."""
     extract_base_folder = os.path.dirname(archive_path)
+    base_name = os.path.basename(archive_path)
+    base_name_no_ext = os.path.splitext(base_name)[0]
+    clean_folder_name = base_name_no_ext + "_clean"
+    clean_folder_path = os.path.join(extract_base_folder, clean_folder_name)
+
+    if os.path.exists(clean_folder_path):
+        shutil.rmtree(clean_folder_path)
+
     with tarfile.open(archive_path, "r:gz") as tar:
-        tar.extractall(path=extract_base_folder)
-        # Assuming the first member is the root directory
-        root_directory = tar.getmembers()[0].name.split('/')[0]
-    supportconfig_folder = os.path.join(extract_base_folder, root_directory)
-    logger.info(f"Extracted .tgz to: {supportconfig_folder}")
-    report_files = walk_supportconfig(supportconfig_folder)
-    return report_files
+        members = tar.getmembers()
+        for member in members:
+            member.name = os.path.join(clean_folder_name, os.path.basename(member.name))
+            tar.extract(member, path=extract_base_folder)
+
+    report_files = walk_supportconfig(clean_folder_path)
+    logger.info(f"Extracted .tgz to: {clean_folder_path}")
+    return report_files, clean_folder_path
+
+def create_txz(source_dir, output_filename):
+    with tarfile.open(output_filename, 'w:xz') as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
