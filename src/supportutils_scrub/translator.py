18e792e97c5f59626f7cf8719553408cd6dc01a8
Ronald Pina <rpina@suse.com>
2024-08-27 17:13:01 +0200

    Fixing an issue in translator.py

--- src/supportutils_scrub/translator.py
+++ src/supportutils_scrub/translator.py
@@ -1,6 +1,8 @@
-# scrubber/translator.py
+# translator.py
 
+import os
 import json
+import logging
 
 class Translator:
     @staticmethod
@@ -8,25 +10,35 @@ class Translator:
         """
         Save translation dictionary to a JSON file.
         """
+        # Ensure the directory exists before saving the file
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        
         with open(file_path, 'w') as json_file:
-            json.dump(translation_dict, json_file, indent = 4)
+            json.dump(translation_dict, json_file, indent=4)
 
     @staticmethod
     def load_datasets_mappings(file_path):
-
+        """
+        Load dataset mappings from a JSON file.
+        """
         try:
             with open(file_path, 'r') as file:
                 return json.load(file)
         except FileNotFoundError:
             # Handle the case where the mappings file doesn't exist.
-            # This could involve returning an empty dictionary or raising an error.
+            # Return an empty dictionary
             return {}
-        
 
     @staticmethod
     def save_datasets(file_path, dataset_dict):
         """
         Save aggregated translation dictionaries to a JSON file.
         """
-        with open(file_path, 'w') as json_file:
-            json.dump(dataset_dict, json_file, indent=4)        
+        # Ensure the directory exists before saving the file
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        
+        try:
+            with open(file_path, 'w') as json_file:
+                json.dump(dataset_dict, json_file, indent=4)
+        except Exception as e:
+            logging.error(f"Failed to save datasets to {file_path}: {e}")
