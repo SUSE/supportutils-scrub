4433bc5216982fbe6da9f69ca4524c73f8bbb1ca
Ronald Pina <rpina@suse.com>
2026-04-23 17:41:53 +0200

    bump spec to 1.5, add SLES16 /usr/etc config path, update changelog

--- src/supportutils_scrub/config.py
+++ src/supportutils_scrub/config.py
@@ -1,5 +1,16 @@
 # config.py
 
-# Default configuration path
-DEFAULT_CONFIG_PATH = "/etc/supportutils-scrub/supportutils-scrub.conf"
+import os as _os
+
+# /etc takes priority (administrator override).
+# /usr/etc is the vendor layer on SLES 16 and later.
+_CANDIDATES = [
+    "/etc/supportutils-scrub/supportutils-scrub.conf",
+    "/usr/etc/supportutils-scrub/supportutils-scrub.conf",
+]
+
+DEFAULT_CONFIG_PATH = next(
+    (p for p in _CANDIDATES if _os.path.exists(p)),
+    _CANDIDATES[0],
+)
 
